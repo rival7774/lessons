6901d74acf4609aa3c324627b2d7cf6691f0770f
Samir <sakitov@yandex.ru>
2020-09-26 21:24:11 +0300

    lesson_js

---
+++ script.js
@@ -0,0 +1,187 @@
+"use strict";
+
+// let number = 5;
+
+// console.log(4/0);
+// console.log('string' * 9);
+
+// const persone = 'Alex';
+// console.log(persone);
+
+// const bool = true;
+
+// let und;
+// console.log(und);
+
+// const obj = {
+// 	name: 'John',
+// 	age: 25, 
+// 	isMarried: false,
+// };
+
+// console.log(obj.name);
+// console.log(obj['name']);
+
+// let arr = ['plum.png', 'orange.jpeg', 6, 'apple.bmp'];
+// console.log(arr[1]);
+
+// const result = confirm('Are you here?');
+// console.log (result);
+
+// const answer = +prompt('Вам есть 18?','18');
+// console.log (answer + 5);
+
+// const answers = [];
+
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Как ваше фамилия?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
+
+// console.log (typeof(answers));
+// console.log (typeof(null));
+
+// const category = 'toys';
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = 'Ivan';
+// alert(`Привет, ${user}`);
+
+console.log('arr' + '-object');
+console.log(4 + +'5');   //  унарный плюс который ставится перед оргументом
+
+let incr = 10,
+	decr = 10;
+
+incr++;
+decr--;	
+
+console.log(incr);
+console.log(decr);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
